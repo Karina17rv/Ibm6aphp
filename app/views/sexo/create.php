812dbf250b234d2de5673f9a458e1b2b05f5d1c2
KARINA RENTERÍA <112963978+Karina17rv@users.noreply.github.com>
2025-04-22 16:38:17 -0500

    aumente bonton create.php
    aumente el boton de apellido

--- app/views/sexo/create.php
+++ app/views/sexo/create.php
@@ -1,5 +1,6 @@
 <form action="../../controllers/SexoController.php?action=create" method="POST">
     <label for="nombre">Nombre:</label>
+    <label for="apellido">Apellido:</label>    
     <input type="text" name="nombre" id="nombre" required>
     <input type="submit" value="Crear">
 </form>
